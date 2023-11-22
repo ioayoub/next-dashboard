465807e9b8581dcf138fdaf35066b8425c008940
IDRISSI O. Ayoub <idrissio.ayoub@gmail.com>
2023-11-22 09:49:42 +0100

    end chapter 9

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import { lusitana } from './ui/fonts';
 import Image from 'next/image';
 
