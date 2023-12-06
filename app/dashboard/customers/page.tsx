730729eaab40a6782fa7f9db15d70f6bd9433875
IDRISSI O. Ayoub <idrissio.ayoub@gmail.com>
2023-12-06 13:32:21 +0100

    add: customers pages logic

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,35 @@
-export default function Page() {
-  return <p>Customers pages</p>;
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import { Metadata } from 'next';
+import { Suspense } from 'react';
+import CustomersTable from '@/app/ui/customers/table';
+import { FormattedCustomersTable } from '@/app/lib/definitions';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+
+  console.log('query', query);
+
+  const customers: FormattedCustomersTable[] =
+    await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <Suspense fallback={<InvoicesTableSkeleton />}>
+          <CustomersTable customers={customers} />
+        </Suspense>
+      </div>
+    </div>
+  );
 }
